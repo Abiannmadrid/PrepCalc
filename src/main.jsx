5eb7bedcfd86d151965d249796a5ccd19e698985
Abiannmadrid <abbyannmadrid@gmail.com>
2025-11-29 02:23:39 -0600

    changed rounting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import Landing from './Landing.jsx'
-import App from './App.jsx'   // your calculator
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/calc" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
 
