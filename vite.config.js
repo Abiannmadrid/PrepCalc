eddbe29518bc062cfcae39cab6b6c273a33b6013
Abiannmadrid <abbyannmadrid@gmail.com>
2025-11-30 19:28:36 -0600

    changed file structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // Use relative paths instead of '/app/'
-})
+
+  // This tells Vercel + Vite to serve the React app from /app
+  base: "/app/",
+
+  build: {
+    outDir: "dist/app", // Put built files into /dist/app
+    emptyOutDir: false, // Don't delete your landing page build
+  },
+});
