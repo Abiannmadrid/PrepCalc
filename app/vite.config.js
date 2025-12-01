48e9ab90bea13f2d23be3bc11761568a7d064ea9
Abiannmadrid <abbyannmadrid@gmail.com>
2025-12-01 14:44:21 -0600

    changed vite.config.js to tell vite to look in app folder

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
+  root: './app'  // Tell Vite to look in the app folder
 })
 
