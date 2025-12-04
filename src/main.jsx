5589652d707cb7c11bb2dd2cc5515a703b55dd87
Abiannmadrid <abbyannmadrid@gmail.com>
2025-12-04 04:05:33 -0600

    Fix main.jsx to remove missing Landing import

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './Landing.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/app" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
 
 
